a8b11218e36d99f7aeacf2b498a88111f693fd71
Marcus92111 <30754692+Marcus92111@users.noreply.github.com>
2017-08-13 14:24:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
 console.log('Loaded!');
+function incrementCounter(){
+  var request =  new XMLHttpRequest();
+  request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE ){
+      if(request.status === 200){
+        var count = getElementById('count');
+        count.innerHTML = request.responseText;
+      }
+    }
+  };
+
+  request.open('GET','http://localhost:8080/counter',true);
+  request.send(null);
+}
+
